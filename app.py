2ce58b302ae13da4f649128cde9fc53124c34587
Yash Chouhan <48046446+iamyashchouhan@users.noreply.github.com>
2024-05-01 18:58:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from gradio_client import Client
-
 
 app = Flask(__name__)
 
@@ -9,20 +7,17 @@ app = Flask(__name__)
 def mbsa():
     return render_template('index.html')
 
-@app.route('/get_response', methods=['POST'])
+@app.route('/get_response', methods=['GET'])
 def get_response():
     # Get data from the request
-    data = request.json
-
-    # Extract parameters from the request data
-    prompt = data.get("prompt")
-    negative_prompt = data.get("negative_prompt")
-    use_negative_prompt = data.get("use_negative_prompt")
-    seed = data.get("seed")
-    width = data.get("width")
-    height = data.get("height")
-    guidance_scale = data.get("guidance_scale")
-    randomize_seed = data.get("randomize_seed")
+    prompt = request.args.get("prompt")
+    negative_prompt = request.args.get("negative_prompt")
+    use_negative_prompt = request.args.get("use_negative_prompt")
+    seed = request.args.get("seed")
+    width = request.args.get("width")
+    height = request.args.get("height")
+    guidance_scale = request.args.get("guidance_scale")
+    randomize_seed = request.args.get("randomize_seed")
 
     # Make prediction using Gradio Client
     client = Client("https://ddosxd-realvisxl.hf.space/--replicas/flm7z/")
@@ -40,5 +35,3 @@ def get_response():
 
     # Return the result as JSON response
     return jsonify(result)
-
-
